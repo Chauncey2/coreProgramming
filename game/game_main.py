eb77edeccf7adb8a764fed485b5ae229445a6ed9
Chauncey2 <gaochangju95@163.com>
2020-02-15 17:55:36 +0800

    飞机大战游戏- 玩家飞机正常起飞

--- game/game_main.py
+++ game/game_main.py
@@ -26,9 +26,11 @@ class GameWindow(object):
         self.hero_plane = hero_plane.HeroPlane()
 
     def run(self):
+
         while True:
             self.__action()
             self.__draw()
+            self.__event()
             self.__update()
 
     def __action(self):
@@ -37,6 +39,10 @@ class GameWindow(object):
         :return:
         """
         self.game_map.move_down()
+        # 遍历子弹
+        for bullet in self.hero_plane.bullet_list:
+            if bullet.is_shot:
+                bullet.move_up()
 
     def __draw(self):
         """
@@ -50,6 +56,47 @@ class GameWindow(object):
         # 添加英雄飞机
         self.window.blit(self.hero_plane.img, (self.hero_plane.img_rect[0], self.hero_plane.img_rect[1]))
 
+        # 添加子弹
+        for bul in self.hero_plane.bullet_list:
+            if bul.is_shot:
+                self.window.blit(bul.bullet_img, (bul.bullet_img_rect[0], bul.bullet_img_rect[1]))
+
+    def __event(self):
+        """
+        处理窗口的监听事件
+        :return:
+        """
+        # 获取所有窗口的事件监听
+        event_list = pygame.event.get()
+        for event in event_list:
+            # 鼠标单击关闭事件
+            if event.type == pygame.QUIT:
+                self.game_over()
+
+            # 监听按下事件
+            if event.type == pygame.KEYDOWN:
+                # 是否按下的是Esc
+                if event.key == pygame.K_ESCAPE:
+                    self.game_over()
+                # 是否按下的是空格
+                if event.key == pygame.K_SPACE:
+                    self.hero_plane.shoot()
+
+        # 监听长按事件 -> 元组(0没按下，1长按了) 字母对应阿斯克码
+        pressed_keys = pygame.key.get_pressed()
+
+        if pressed_keys[pygame.K_UP] or pressed_keys[pygame.K_w]:
+            self.hero_plane.move_up()
+
+        if pressed_keys[pygame.K_DOWN] or pressed_keys[pygame.K_s]:
+            self.hero_plane.move_down()
+
+        if pressed_keys[pygame.K_LEFT] or pressed_keys[pygame.K_a]:
+            self.hero_plane.move_left()
+
+        if pressed_keys[pygame.K_RIGHT] or pressed_keys[pygame.K_d]:
+            self.hero_plane.move_right()
+
     def __update(self):
         """
         刷新页面
@@ -57,11 +104,8 @@ class GameWindow(object):
         """
         pygame.display.update()
 
+    def game_over(self):
+        pygame.quit()
+        sys.exit()
 
-def start():
-    game_window = GameWindow()
-    game_window.run()
 
-
-if __name__ == '__main__':
-    start()
--- game/hero_plane.py
+++ game/hero_plane.py
@@ -1,10 +1,12 @@
 import pygame
+from game import bullet
 
 WINDOW_WIDTH = 512
 WINDOW_HEIGHT = 768
 
 
 class HeroPlane(object):
+
     def __init__(self):
         # 飞机图片
         self.img = pygame.image.load('res/images/hero2.png')
@@ -16,18 +18,36 @@ class HeroPlane(object):
         self.speed = 3
 
         # 子弹弹夹
+        self.bullet_list = [bullet.Bullet() for _ in range(6)]
 
-        def move_up(self):
-            pass
+    def move_up(self):
+        # 边界检查
+        if self.img_rect[1] >= 0:
+            self.img_rect.move_ip(0, -self.speed)  # move_ip(x.y)
 
-        def move_down(self):
-            pass
+    def move_down(self):
+        # 边界检查
+        if self.img_rect[1] <= WINDOW_HEIGHT - self.img_rect[3]:
+            self.img_rect.move_ip(0, self.speed)  # move_ip(x.y)
 
-        def move_left(self):
-            pass
+    def move_left(self):
+        # 边界检查
+        if self.img_rect[0] >= 0:
+            self.img_rect.move_ip(-self.speed, 0)  # move_ip(x.y)
 
-        def move_right(self):
-            pass
+    def move_right(self):
+        # 边界检查
+        if self.img_rect[0] <= WINDOW_WIDTH - self.img_rect[2]:
+            self.img_rect.move_ip(self.speed, 0)  # move_ip(x.y)
 
-        def shoot(self):
-            pass
+    def shoot(self):
+        # 遍历所有子弹
+        for bul in self.bullet_list:
+            if not bul.is_shot:
+                # 设置子弹发射位置
+                bul.bullet_img_rect[0] = self.img_rect[0] + (self.img_rect[2] - bul.bullet_img_rect[2]) / 2
+                bul.bullet_img_rect[1] = self.img_rect[1] - (bul.bullet_img_rect[3] - 10)
+                # 更新子弹状态
+                bul.is_shot = True
+                # 一次只能发射一颗子弹
+                break
