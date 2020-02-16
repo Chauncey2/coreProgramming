fa21d82ffea49c87882f953cf9dec85c0210b8c3
Chauncey2 <gaochangju95@163.com>
2020-02-16 19:22:16 +0800

    飞机大战游戏- 添加敌机和击落音效以及背景音乐

--- game/game_main.py
+++ game/game_main.py
@@ -1,4 +1,4 @@
-from game import game_map, hero_plane
+from game import game_map, planes
 import pygame
 import sys
 
@@ -23,7 +23,16 @@ class GameWindow(object):
         # 创建地图实例
         self.game_map = game_map.GameMap()
         # 创建玩家飞机
-        self.hero_plane = hero_plane.HeroPlane()
+        self.hero_plane = planes.HeroPlane()
+
+        # 创建一波敌机
+        self.enemy_list = [planes.EnemyPlane() for _ in range(6)]
+
+        # 添加背景音乐
+        pygame.mixer_music.load('res/musics/bg2.ogg')
+        pygame.mixer_music.play(-1)
+        # 加载碰撞音效
+        self.boom_sound = pygame.mixer.Sound('res/musics/bomb.wav')
 
     def run(self):
 
@@ -32,6 +41,7 @@ class GameWindow(object):
             self.__draw()
             self.__event()
             self.__update()
+            self.__hit_enemy()
 
     def __action(self):
         """
@@ -43,6 +53,10 @@ class GameWindow(object):
         for bullet in self.hero_plane.bullet_list:
             if bullet.is_shot:
                 bullet.move_up()
+
+        # 敌机做自由落体运动
+        for enemy in self.enemy_list:
+            enemy.move_down()
 
     def __draw(self):
         """
@@ -60,6 +74,10 @@ class GameWindow(object):
         for bul in self.hero_plane.bullet_list:
             if bul.is_shot:
                 self.window.blit(bul.bullet_img, (bul.bullet_img_rect[0], bul.bullet_img_rect[1]))
+
+        # 添加敌机
+        for enemy in self.enemy_list:
+            self.window.blit(enemy.img, (enemy.img_rect[0], enemy.img_rect[1]))
 
     def __event(self):
         """
@@ -104,8 +122,28 @@ class GameWindow(object):
         """
         pygame.display.update()
 
+    def __hit_enemy(self):
+        """
+        碰撞检测
+        :return:
+        """
+        for bullet in self.hero_plane.bullet_list:
+            if bullet.is_shot:
+                for enemy in self.enemy_list:
+                    is_hit = pygame.Rect.colliderect(bullet.bullet_img_rect, enemy.img_rect)
+                    if is_hit:
+                        bullet.is_shot = False
+                        enemy.reset()
+                        # 播放爆炸音效
+                        self.boom_sound.play()
+                        # 更新分数
+
     def game_over(self):
+        # 停止背景音乐
+        pygame.mixer.music.stop()
+        # 停止音效
+        self.boom_sound.stop()
+        # 停止游戏引擎
         pygame.quit()
+        # 关闭窗口，退出游戏，
         sys.exit()
-
-
--- game/planes.py
+++ game/planes.py
@@ -1,11 +1,12 @@
 import pygame
 from game import bullet
+import random
 
 WINDOW_WIDTH = 512
 WINDOW_HEIGHT = 768
 
 
-class HeroPlane(object):
+class HeroPlane:
 
     def __init__(self):
         # 飞机图片
@@ -51,3 +52,40 @@ class HeroPlane(object):
                 bul.is_shot = True
                 # 一次只能发射一颗子弹
                 break
+
+
+class EnemyPlane:
+
+    def __init__(self):
+        # 随机加载敌机图片
+        self.num = str(random.randint(1, 7))
+        self.img = pygame.image.load('res/images/img-plane_' + self.num + '.png')
+
+        # 敌机的外框矩形
+        self.img_rect = self.img.get_rect()
+        # 敌机的初始位置
+        self.img_rect.move_ip((WINDOW_WIDTH - self.img_rect[2]) / 2, 0)
+        # 敌机的移动速度
+        self.speed = 3
+        # 随机初始化位置
+        self.reset()
+
+    def move_down(self):
+        """
+        敌机向下移动
+        :return:
+        """
+        self.img_rect.move_ip(0, self.speed)
+
+        # 敌机位置回收
+        if self.img_rect[1] >= WINDOW_HEIGHT:
+            self.reset()
+
+    def reset(self):
+        """
+        敌机初始化位置
+        :return:
+        """
+        self.img_rect[0] = random.randint(0, WINDOW_WIDTH - self.img_rect[2])
+        self.img_rect[1] = self.img_rect[3]
+        self.speed = random.randint(3, 5)
