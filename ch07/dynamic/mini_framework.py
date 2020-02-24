dfb7df884dddb4e52e88373900878ba9389d58a0
Chauncey2 <gaochangju95@163.com>
2020-02-24 19:11:01 +0800

    更新

---
+++ ch07/dynamic/mini_framework.py
@@ -0,0 +1,88 @@
+import time
+import re
+
+
+def index():
+    """
+    打开对应的模板页
+    :return: html
+    """
+    with open("../templates/index.html",encoding='utf-8') as f:
+        html_content = f.read()
+    return html_content
+
+
+def center():
+    """
+    打开内容页
+    :return: html page
+    """
+    with open("../templates/center.html", encoding="utf-8") as f:
+        html_content = f.read()
+    data_from_mysql = "这里是从mysql中查询出来的数据"
+    html_content = re.sub(r"\{%content%\}", data_from_mysql, html_content)
+
+    return html_content
+
+
+def register():
+    return "-----注册页面----current time is %s" % time.ctime()
+
+def login():
+    return "-----登陆页面----current time is %s" % time.ctime()
+
+
+def unregister():
+    return "-----注销页面----current time is %s" % time.ctime()
+
+# 路由：根据不一样的请求，不一样的函数去服务器
+# key是浏览器中可能输入的url
+# value是url需要调用的函数的引用
+# 通过这个字典，做到了只要添加一行 key-value就完成了 对应服务的设定
+url_func_dict = {
+    "/index.py": index,
+    "/center.py": center,
+    "/register.py": register,
+    "/login.py": login,
+    "/unregister.py": unregister
+}
+
+
+def application(env, set_header):
+    # 1. 调用set_header指向的函数，将response_header传递过去
+    status = '200 OK'
+    response_headers = [('Content-Type', 'text/html; charset=UTF-8')]
+    set_header(status, response_headers)
+
+    # 提取url
+    path_info = env['PATH_INFO']  # /index.py
+    try:
+        # url不一样，那么取出来的value，即函数的引用不一样
+        func = url_func_dict[path_info]  # 如果path_info是/index.py那么也就意味着取 index函数的引用
+        # 那么将来调用的时候，就调用了不一样的函数
+        response_body = func()
+    except Exception as ret:
+        response_body = "-----not found you page-----"
+
+    """
+    if path_info == "/index.py":
+        response_body = index()
+    elif path_info == "/center.py":
+        response_body = center() 
+    elif path_info == "/register.py":
+        response_body = register()
+    elif path_info == "/login.py":
+        response_body = login()
+    else:
+        response_body = "-----not found you page-----"
+    """
+
+    # 2. 通过return 将body返回
+    return response_body
+
+
+
+if __name__ == '__main__':
+    # print(index())
+    print(center())
+    pass
---
+++ ch07/web_server.py
@@ -0,0 +1,113 @@
+import socket
+import multiprocessing
+import re
+from ch07.dynamic import mini_framework
+
+BASE_URL=''
+
+class WSGIServer:
+    def __init__(self):
+        """
+        初始化功能，创建套接字/绑定等
+        """
+        # 创建一个服务器套接字
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 套接字 地址重用选项 1设置0取消
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # 绑定
+        self.server_socket.bind(('', 9999))
+        # 监听，被动套接字，设置已完成三次握手队列长度
+        self.server_socket.listen(128)
+
+    def request_handler(self, client_socket):
+        """
+        处理客户请求
+        :param client_socket:
+        :return:
+        """
+        recv_data = client_socket.recv(4096)
+        if not recv_data:
+            print("连接断开")
+            client_socket.close()
+            return
+        # 对接收的数据进行解码
+        request_str_data = recv_data.decode()
+        # 通过正则表达式提取数据
+        ret = re.match(r"[^/]+([^ ]+)", request_str_data)
+        if ret:
+            path_info = ret.group(1)  # /index.html
+            print(">" * 30, path_info)
+        else:
+            path_info = "/"
+        print("用户请求路径是%s" % path_info)
+        if path_info == '/':
+            path_info = '/index.html'
+
+        # 用if判断来区分动态请求/静态请求
+        if not path_info.endswith(".py"):
+            # 静态请求
+            try:
+                if path_info.__contains__("html"):
+                    with open('./templates/' + path_info, "rb") as f:
+                        file_data = f.read()
+                else:
+                    with open('./' + path_info, "rb") as f:
+                        file_data = f.read()
+            except:
+                response_line = "HTTP/1.1 404 Not Found\r\n"
+                response_header = "Server: PythonWebServer2.0\r\n"
+                response_body = "ERROR!!!!!"
+                response_data = response_line + response_header + "\r\n" + response_body
+                client_socket.send(response_data.encode())
+            else:
+                response_header = "HTTP/1.1 200 OK\r\n"
+                response_header += "Server: PythonWebServer1.0\r\n"
+                response_header += "\r\n"
+                response_body = file_data
+
+                # 拼接报文
+                response = response_header.encode("utf-8") + response_body
+                # 发送
+                client_socket.send(response)
+            finally:
+                client_socket.close()
+        else:
+            # 动态请求
+            env = dict()
+            env["PATH_INFO"] = path_info
+            response_body = mini_framework.application(env, self.set_headers)
+            # 拼接response
+            response = self.response_header + response_body
+            client_socket.send(response.encode("utf-8"))
+
+    def set_headers(self, status, headers):
+        print("-----web_server.py set_headers 被调用-----")
+        # status ---> 200 OK
+        # headers--->[("Content-Type", "text/html;")]
+        response_header = "HTTP/1.1 %s\r\n" % status
+        for temp in headers:
+            response_header += "%s: %s\r\n" % (temp[0], temp[1])
+        response_header += "\r\n"
+        self.response_header = response_header
+
+    def run(self):
+        """
+        等待客户连接，然后创建子进程为客户服务
+        :return:
+        """
+        while True:
+            # 从队列中取出一个客户套接字用以服务
+            client_socker, client_addr = self.server_socket.accept()
+            p = multiprocessing.Process(target=self.request_handler, args=(client_socker,))
+            p.start()
+            client_socker.close()
+
+
+def main():
+    # 创建一个server服务对象
+    wsgi_server = WSGIServer()
+    wsgi_server.run()
+
+
+if __name__ == '__main__':
+    main()
